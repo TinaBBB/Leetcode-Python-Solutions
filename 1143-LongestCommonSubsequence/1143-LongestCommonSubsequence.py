cb778df908410d28d5e702edb48d79e111cc77d8
Tianshu Shen <tinashen.tianshu@gmail.com>
2024-10-31 23:49:05 -0400

    Adding solution

---
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # oob have 0 paddings
+        dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+
+        # Loop through rows from the bottom and columns from the right
+        for row_idx in range(len(text1)-1, -1, -1):
+            for col_idx in range(len(text2)-1, -1, -1):
+                # match, go diagonal
+                if text1[row_idx] == text2[col_idx]:
+                    dp[row_idx][col_idx] = 1 + dp[row_idx+1][col_idx+1]
+                # not match, check max of right and bottom values
+                else:
+                    dp[row_idx][col_idx] = max(
+                        dp[row_idx][col_idx+1], dp[row_idx+1][col_idx]
+                        )
+
+        return dp[0][0]
+
+# Time: O(mn) to compute the grid
+# Memory: O(mn) to store the grid
