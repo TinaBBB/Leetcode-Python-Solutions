18d657f787a353d965189fa2a30c5b824bffc140
Tianshu Shen <tinashen.tianshu@gmail.com>
2024-11-16 17:56:47 -0500

    Create file

---
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # Conventional DP problem
+        # The last state is at the last index, where its LIS have a length of 1.
+        # Last the second-last index, the longest sequence would be either including just itself or 
+        # including itself + the LIS of some later indecies that have a number greater than it.
+
+        # Memoization initialization
+        LIS = [1] * len(nums)
+
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i+1, len(nums)):
+                # update only if eligible
+                if nums[j] > nums[i]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+        
+        return max(LIS)
+        
