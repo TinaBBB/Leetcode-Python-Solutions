1f5fc28427d4f7d4aa1965ba6441badb4934c72f
Tianshu Shen <tinashen.tianshu@gmail.com>
2024-11-26 22:08:12 -0500

    Update file 11/26/2024, 10:08:11 PM

--- 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
@@ -1,18 +1,26 @@
 class Solution:
     
-    # LCS solution
+    # LCS solution, of string itself, and its reverse
     def longestPalindromeSubseq(self, s: str) -> int:
         return self.longestCommonSubsequence(s, s[::-1])
         
-    
-    def longestCommonSubsequence(self, s1: str, s2: str) -> int:
-        N, M = len(s1), len(s2)
-        dp = [[0] * (M+1) for _ in range(N+1)]
+    # Time: O(mn) to compute the grid
+    # Memory: O(mn) to store the grid    
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+            # oob have 0 paddings
+            dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
 
-        for i in range(N):
-            for j in range(M):
-                if s1[i] == s2[j]:
-                    dp[i+1][j+1] = 1 + dp[i][j]
-                else:
-                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
-        return dp[N][M]
+            # Loop through rows from the bottom and columns from the right
+            for row_idx in range(len(text1)-1, -1, -1):
+                for col_idx in range(len(text2)-1, -1, -1):
+                    # match, go diagonal
+                    if text1[row_idx] == text2[col_idx]:
+                        dp[row_idx][col_idx] = 1 + dp[row_idx+1][col_idx+1]
+                    # not match, check max of right and bottom values
+                    else:
+                        dp[row_idx][col_idx] = max(
+                            dp[row_idx][col_idx+1], dp[row_idx+1][col_idx]
+                            )
+
+            return dp[0][0]
+
